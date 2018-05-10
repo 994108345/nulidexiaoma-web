e9f76fc87f3d3228866f2d0856c469a55be36fb4
wenzailong <994108345@qq.com>
2018-05-10 17:57:29 +0800

    日常提交

--- src/app/nulidexiaoma/module/jquery/component/main.component.ts
+++ src/app/nulidexiaoma/module/jquery/component/main.component.ts
@@ -1,7 +1,6 @@
 import {Component, Injector, OnInit} from '@angular/core';
 import {AbstractComponent} from "../../../../base/common/abstract.component";
-import {$} from "protractor";
-
+import * as $ from 'jquery';
 @Component({
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
@@ -14,8 +13,39 @@ export class MainComponent extends AbstractComponent implements OnInit{
   ngOnInit(): void {
   }
 
-  jqueryIsExi(){
-    $("p1").text="wwwwwwwwwwwwwwwwww";
+  jqueryHide(){
+   $("#p1").toggle("fast",);
   }
+  jqueryShow(){
+    $('#p1').show();
+  }
+  //淡入
+  fade(){
+    $("#p2").fadeToggle("slow");
+  }
+  //滑动
+  slideDown(){
+    $("#p3").slideDown("slow");
+  }
+  slideUp(){
+    $("#p3").slideUp("slow");
+  }
+  slideToggle(){
+    $("#p3").slideToggle("slow");
+  }
+
+  animate(){
+    $("#p4").animate({left:'100px'},"slow");
+    $("#p4").animate({fontSize:'3em'},"slow");
+    $("#p4").animate({left:'500'},"slow");
+    $("#p4").animate({right:'50'},"slow");
+    $("#p4").animate({fontSize:'2em'},"slow");
+    $("#p4").animate({right:'100px'},"5000");
+  }
+  fun1(){
+
+  }
+
+
 
 }
---
+++ src/app/nulidexiaoma/module/jquery/jquery.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import {MainComponent} from './component/main.component';
+import {FormsModule} from '@angular/forms';
+
+import {
+  ButtonModule, GrowlModule, InputTextModule,
+  MessagesModule
+} from 'primeng/primeng';
+import {HttpModule} from '@angular/http';
+import {MessageService} from 'primeng/components/common/messageservice';
+import {Router, RouterModule} from '@angular/router';
+import {routing} from './jquery.component.routing';
+import {CommonService} from "../../../base/service/common/common.service";
+import {WzlAlertService} from "../../../base/service/wzlalert/wzlalert.service";
+import {CommonModule} from "@angular/common";
+import {WzlCacheService} from "../../../base/service/wzlcache/wzlceche.service";
+import {JqueryComponent} from "./jquery.component";
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    InputTextModule,
+    MessagesModule,
+    GrowlModule,
+    FormsModule,
+    ButtonModule,
+    HttpModule,
+    RouterModule,
+    InputTextModule,
+    routing
+  ],
+  declarations: [
+    MainComponent,
+    JqueryComponent,
+  ],
+  providers: [CommonService, WzlAlertService, MessageService,WzlCacheService],
+})
+export class JqueryModule { }
